5b3eb3484df9b80035d3c31ad148499f61d42070
umbralav <umbral.av@gmail.com>
2016-07-26 18:57:54 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(document).on('click', 'a', function(event){
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 1000);
+});
+
+//Form
+jQuery(document).ready(function() {
+	jQuery("#contacto").submit(function(e) {
+	    jQuery.ajax({
+	           	url: "https://formspree.io/umbral.av@gmail.com", 
+   				method: "POST",
+    			data: jQuery("#contacto").serialize(), // serializes the form's elements.
+    			dataType: "json",
+     			success: function()
+	           {
+	              jQuery("#contacto").addClass("displaynone");
+	              jQuery("#text_thankyou").addClass("displayblock");
+	           }
+	         });
+
+	   		e.preventDefault(); // avoid to execute the actual submit of the form.
+	});
+});
